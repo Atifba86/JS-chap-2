b1da716f9bfead450bc59fe51fe598c4b1a5dee0
Atifba86 <70482315+Atifba86@users.noreply.github.com>
2021-06-06 18:16:16 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+//task 1 & 2
+// var username = prompt("Enter your First Name: ")
+// var myname = prompt("Enter Your Second Name: ")
+// document.write(username + myname)
+
+// task 3
+// var message= "hello world"
+// alert(message)
+
+//task 4
+// var name1= prompt("Enter Your Full name please: ")
+// var age= prompt("Enter Your Age: ")
+// alert(name1)
+// alert(age)
+// alert("Certified Mobile Application Development ")
+
+//task 5
+// alert("Pizza\npizz\npiz\npi\np")
+
+//task 6
+// var email=prompt("Enter Your Email: ")
+// var end = "@gmail.com"
+// alert(email + end)
+
+//task 7
+// var book=("A smarter way to learn JavaScript")
+// alert(book)
+
+//task 8
+// document.write("Yah! I can write HTML content through javaScript")
+
+//task 9
+// var show="(▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬)"
+// alert(show)
